2028cf5d3842ddba1fab1fc6c7210656000c9295
Pahud <pahudnet@gmail.com>
2020-12-20 13:15:35 +0800

    minor

--- .projenrc.js
+++ .projenrc.js
@@ -1,33 +1,33 @@
 const {
   AwsCdkTypeScriptApp,
   GithubWorkflow,
-  Semver
+  Semver,
 } = require('projen');
 
 const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
 
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: "1.77.0",
-  name: "fargate-global",
-  authorName: "Pahud Hsieh",
-  authorEmail: "pahudnet@gmail.com",
-  repository: "https://github.com/pahud/fargate-global.git",
+  cdkVersion: '1.77.0',
+  name: 'fargate-global',
+  authorName: 'Pahud Hsieh',
+  authorEmail: 'pahudnet@gmail.com',
+  repository: 'https://github.com/pahud/fargate-global.git',
   dependabot: false,
   cdkDependencies: [
-    "@aws-cdk/aws-certificatemanager",
-    "@aws-cdk/aws-ec2",
-    "@aws-cdk/aws-ecs",
-    "@aws-cdk/aws-ecs-patterns",
-    "@aws-cdk/aws-globalaccelerator",
-    "@aws-cdk/aws-route53",
-    "@aws-cdk/aws-route53-patterns",
-    "@aws-cdk/aws-route53-targets",
-    "@aws-cdk/core",
-  ]
+    '@aws-cdk/aws-certificatemanager',
+    '@aws-cdk/aws-ec2',
+    '@aws-cdk/aws-ecs',
+    '@aws-cdk/aws-ecs-patterns',
+    '@aws-cdk/aws-globalaccelerator',
+    '@aws-cdk/aws-route53',
+    '@aws-cdk/aws-route53-patterns',
+    '@aws-cdk/aws-route53-targets',
+    '@aws-cdk/core',
+  ],
 });
 
 project.addDependencies({
-  "cdk-remote-stack": Semver.caret('0.1.38'),
+  'cdk-remote-stack': Semver.caret('0.1.38'),
 });
 
 
@@ -36,7 +36,7 @@ workflow = project.github.addWorkflow('ProjenYarnUpgrade');
 
 workflow.on({
   schedule: [{
-    cron: '11 0 * * *'
+    cron: '11 0 * * *',
   }], // 0:11am every day
   workflow_dispatch: {}, // allow manual triggering
 });
@@ -46,16 +46,16 @@ workflow.addJobs({
     'runs-on': 'ubuntu-latest',
     'steps': [
       { uses: 'actions/checkout@v2' },
-      { 
+      {
         uses: 'actions/setup-node@v1',
         with: {
           'node-version': '10.17.0',
-        }
+        },
       },
       // { run: `yarn install` },
       // { run: `yarn projen` },
-      { run: `yarn upgrade` },
-      { run: `yarn projen:upgrade` },
+      { run: 'yarn upgrade' },
+      { run: 'yarn projen:upgrade' },
       // submit a PR
       {
         name: 'Create Pull Request',
@@ -67,12 +67,11 @@ workflow.addJobs({
           'title': 'chore: upgrade projen and yarn',
           'body': 'This PR upgrades projen and yarn upgrade to the latest version',
           'labels': 'auto-merge',
-        }
+        },
       },
     ],
   },
 });
-
 
 
 const common_exclude = ['cdk.context.json', 'docker-compose.yml', 'yarn-error.log'];
